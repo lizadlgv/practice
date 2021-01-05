037583b327ffe4fa855a1b1125c198eba71090f3
lizadlgv <liza.dolgova2002@mail.ru>
2021-01-05 19:44:39 +0400

    1.4 Класс Points и проверка работы его методов

---
+++ src/main/java/ru/ssau/tk/lizadlgv/practice/point/Points.java
@@ -0,0 +1,22 @@
+package ru.ssau.tk.lizadlgv.practice.point;
+
+public class Points {
+    private Points() {
+    }
+
+    public static Point sum(Point firstObj, Point secondObj) {
+        return new Point(firstObj.x + secondObj.x, firstObj.y + secondObj.y, firstObj.z + secondObj.z);
+    }
+
+    public static Point subtract(Point firstObj, Point secondObj) {
+        return new Point(firstObj.x - secondObj.x, firstObj.y - secondObj.y, firstObj.z - secondObj.z);
+    }
+
+    public static Point multiply(Point firstObj, Point secondObj) {
+        return new Point(firstObj.x * secondObj.x, firstObj.y * secondObj.y, firstObj.z * secondObj.z);
+    }
+
+    public static Point divide(Point firstObj, Point secondObj) {
+        return new Point(firstObj.x / secondObj.x, firstObj.y / secondObj.y, firstObj.z / secondObj.z);
+    }
+}
---
+++ src/main/java/ru/ssau/tk/lizadlgv/practice/point/PointsInstance.java
@@ -0,0 +1,15 @@
+package ru.ssau.tk.lizadlgv.practice.point;
+
+public class PointsInstance {
+    public static void main(String[] args) {
+        Point firstPoint = new Point(3., 4., 5.);
+        Point secondPoint = new Point(3., 4., 5.);
+        System.out.println("First point = (" + firstPoint.x + ", " + firstPoint.y + ", " + firstPoint.z + ")");
+        System.out.println("Second point = (" + secondPoint.x + ", " + secondPoint.y + ", " + secondPoint.z + ")");
+        
+        System.out.println("Sum = (" + Points.sum(firstPoint, secondPoint).x + ", " + Points.sum(firstPoint, secondPoint).y + ", " + Points.sum(firstPoint, secondPoint).z + ")");
+        System.out.println("Subtract = (" + Points.subtract(firstPoint, secondPoint).x + ", " + Points.subtract(firstPoint, secondPoint).y + ", " + Points.subtract(firstPoint, secondPoint).z + ")");
+        System.out.println("Multiply = (" + Points.multiply(firstPoint, secondPoint).x + ", " + Points.multiply(firstPoint, secondPoint).y + ", " + Points.multiply(firstPoint, secondPoint).z + ")");
+        System.out.println("Divide = (" + Points.divide(firstPoint, secondPoint).x + ", " + Points.divide(firstPoint, secondPoint).y + ", " + Points.divide(firstPoint, secondPoint).z + ")");
+    }
+}
