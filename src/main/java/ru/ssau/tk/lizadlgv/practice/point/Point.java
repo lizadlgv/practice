017f8b0c8bdbf0a331d3306e0f3c8a6c8fedb3e9
lizadlgv <liza.dolgova2002@mail.ru>
2021-01-05 18:55:19 +0400

    1.3 Класс Point + его экземпляры + переписан метод toString

---
+++ src/main/java/ru/ssau/tk/lizadlgv/practice/point/Point.java
@@ -0,0 +1,18 @@
+package ru.ssau.tk.lizadlgv.practice.point;
+
+public class Point {
+    public final double x;
+    public final double y;
+    public final double z;
+
+    public Point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ", " + z + ")";
+    }
+}
---
+++ src/main/java/ru/ssau/tk/lizadlgv/practice/point/PointInstance.java
@@ -0,0 +1,13 @@
+package ru.ssau.tk.lizadlgv.practice.point;
+
+public class PointInstance {
+    public static void main(String[] args) {
+        Point firstPoint = new Point(0., 0., 0.);
+        Point secondPoint = new Point(-8., -10., 5.);
+        Point thirdPoint = new Point(3.25, 100., 59.4);
+
+        System.out.println("firstPoint = " + firstPoint);
+        System.out.println("secondPoint = " + secondPoint);
+        System.out.println("thirdPoint = " + thirdPoint);
+    }
+}
