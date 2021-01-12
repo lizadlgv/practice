6785b3222f88bee3362e04174a2951395ed06680
lizadlgv <liza.dolgova2002@mail.ru>
2021-01-13 02:04:19 +0400

    1.11 Переписаны тесты для класса Person

--- src/test/java/ru/ssau/tk/lizadlgv/practice/person/PersonTest.java
+++ src/test/java/ru/ssau/tk/lizadlgv/practice/person/PersonTest.java
@@ -3,24 +3,45 @@ package ru.ssau.tk.lizadlgv.practice.person;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNull;
 
 public class PersonTest {
+    Person firstObj = new Person();
+    Person secondObj = new Person("Scott", "Pilgrim");
+    Person thirdObj = new Person(null, null, 123456);
+    Person fourthObj = new Person(123456);
+
     @Test
-    public void testMethod() {
-        Person firstObj = new Person();
-        Person secondObj = new Person();
+    public void testFirstNameMethods() {
         firstObj.setFirstName("Anton");
-        secondObj.setFirstName("Ivan");
-        firstObj.setLastName("Ivanov");
-        secondObj.setLastName("Antonov");
-        firstObj.setPassportId(135792);
-        secondObj.setPassportId(864209);
+        secondObj.setFirstName(null);
+        thirdObj.setFirstName("Anna");
 
         assertEquals(firstObj.getFirstName(), "Anton");
-        assertEquals(secondObj.getFirstName(), "Ivan");
-        assertEquals(firstObj.getLastName(), "Ivanov");
-        assertEquals(secondObj.getLastName(), "Antonov");
-        assertEquals(firstObj.getPassportId(), 135792);
-        assertEquals(secondObj.getPassportId(), 864209);
+        assertNull(secondObj.getFirstName());
+        assertEquals(thirdObj.getFirstName(), "Anna");
+        assertNull(fourthObj.getFirstName());
+    }
+
+    @Test
+    public void testLastNameMethods() {
+        thirdObj.setLastName("Akhmatova");
+        fourthObj.setLastName(null);
+
+        assertNull(firstObj.getLastName());
+        assertEquals(secondObj.getLastName(), "Pilgrim");
+        assertEquals(thirdObj.getLastName(), "Akhmatova");
+        assertNull(fourthObj.getLastName());
+    }
+
+    @Test
+    public void testPassportIDMethods() {
+        secondObj.setPassportId(742782);
+        fourthObj.setPassportId(697583);
+
+        assertEquals(firstObj.getPassportId(), 0);
+        assertEquals(secondObj.getPassportId(), 742782);
+        assertEquals(thirdObj.getPassportId(), 123456);
+        assertEquals(fourthObj.getPassportId(), 697583);
     }
 }
