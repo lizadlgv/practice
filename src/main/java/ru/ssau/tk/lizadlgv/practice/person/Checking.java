4622a1847641ec77a06b68f1a434ef300597024b
lizadlgv <liza.dolgova2002@mail.ru>
2021-01-13 15:28:19 +0400

    1.14

--- src/main/java/ru/ssau/tk/lizadlgv/practice/person/Checking.java
+++ src/main/java/ru/ssau/tk/lizadlgv/practice/person/Checking.java
@@ -4,27 +4,17 @@ public class Checking {
     public static void main(String[] args) {
         Person person = new Person();
         person.setFirstName("Arkadiy");
-        int number = 5;
-        checkInt(number);
-        checkPerson(person);
-        System.out.println(number);
+        checkAnotherPerson(person);
         System.out.println(person.getFirstName());
     }
 
-    private static void checkInt(int number) {
-        number = 10;
-        System.out.println(number);
-    }
-
-    private static void checkPerson(Person person) {
-        person.setFirstName("Oleg");
+    private static void checkAnotherPerson(Person person) {
+        person = new Person();
+        person.setFirstName("Ignat");
         System.out.println(person.getFirstName());
     }
 
-    /*Сначала выполняется метод checkInt() и выводится число 10, несмотря на то, что мы передаем в этот метод 5.
-     * Это происходит из-за того, что аргументы-примитивы передаются по значению и их можно редактировать, что внутри метода и
-     * сделано (number = 10). Аналогично с методом checkPerson — только Person это ссылочный тип, в аргументе передается
-     * ссылка на объект person. Внутри main выдаст 5, т.к. копия с number = 10 осталась в методе checkInt, а имя выведет
-     * Oleg, т.к. в метод checkPerson объект person передавался по ссылке и имя изменилось у объекта вообще, созданного в
-     * main-классе*/
+    /*Сначала выводится Игнат, т.к. создается новый объект и присвается ссылке person внутри метода. После
+     * завершения метода измененная ссылка вместе с новым объектом убивается сборщиком мусора и в main-методе выводится
+     * Arkadiy, определённый в main*/
 }
