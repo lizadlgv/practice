b35ba6eef9ef1f48fb1c007c4158f2a688da3f57
lizadlgv <liza.dolgova2002@mail.ru>
2021-01-13 17:57:05 +0400

    1.19 Класс GetNameOfClassOrPrimitive

---
+++ src/test/java/ru/ssau/tk/lizadlgv/practice/GetNameOfClassOrPrimitiveTest.java
@@ -0,0 +1,24 @@
+package ru.ssau.tk.lizadlgv.practice;
+
+import org.testng.annotations.Test;
+import ru.ssau.tk.lizadlgv.practice.person.Person;
+
+import static ru.ssau.tk.lizadlgv.practice.GetNameOfClassOrPrimitive.printType;
+
+public class GetNameOfClassOrPrimitiveTest {
+    @Test
+    public void testPrintType() {
+        printType((byte) 255);
+        printType('z');
+        printType((short) 65535);
+        printType(125);
+        printType(125L);
+        printType(0.1f);
+        printType(0.1d);
+        printType(false);
+        printType(new Person());
+        printType(new Integer(1));
+        printType(new String());
+        printType(null);
+    }
+}
