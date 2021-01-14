1e46a56a2578d718e21fbc3e92a6216c72e8c451
lizadlgv <liza.dolgova2002@mail.ru>
2021-01-15 03:22:44 +0400

    3.1

---
+++ src/main/java/ru/ssau/tk/lizadlgv/practice/strings/MainClass.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.lizadlgv.practice.strings;
+
+public class MainClass {
+    public static void main(String[] args) {
+        String s1 = "aBcDeFgH";
+        MyString MyString = new MyString();
+        MyString.splitLine(s1);
+    }
+}
---
+++ src/main/java/ru/ssau/tk/lizadlgv/practice/strings/MyString.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.lizadlgv.practice.strings;
+
+public class MyString {
+    void splitLine(String myString) {
+        for (int i = 0; i < myString.length(); i++) {
+            System.out.println(myString.charAt(i));
+        }
+    }
+}
